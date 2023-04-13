bd9e22c694483bc0aec96e9dec5289929609d554
PauliusMaslauskas <paulius.mass@gmail.com>
2023-04-13 21:09:34 +0300

    Refactor MainActivity class

--- app/src/main/java/com/example/mytestapplication/MainActivity.java
+++ app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -1,69 +1,77 @@
 package com.example.mytestapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1052;
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnNextActivity = findViewById(R.id.btnNextActivity);
-        Button btnNotify = findViewById(R.id.btnNotification);
-        String channelId = getString(R.string.channel_id);
         createNotificationChannel();
 
+        Button btnNextActivity = findViewById(R.id.btnNextActivity);
+        btnNextActivity.setOnClickListener(v -> openSecondaryActivity());
 
-        btnNextActivity.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
-            startActivity(intent);
-        });
+        Button btnNotify = findViewById(R.id.btnNotification);
+        btnNotify.setOnClickListener(v -> sendNotification());
+    }
+    private void openSecondaryActivity() {
+        Intent intent = new Intent(this, SecondaryActivity.class);
+        startActivity(intent);
+    }
+    private void sendNotification() {
+        String channelId = getString(R.string.channel_id);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(android.R.drawable.star_on)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_text))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setDefaults(NotificationCompat.DEFAULT_VIBRATE);
 
-        btnNotify.setOnClickListener(view -> {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, channelId)
-                    .setSmallIcon(android.R.drawable.star_on)
-                    .setContentTitle("Hello")
-                    .setContentText("This is a notification from My Test App")
-                    .setPriority(NotificationCompat.PRIORITY_MAX)
-                    .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
-                    .setAutoCancel(true);
-                    ;
+        if (hasNotificationPermission()) {
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
+        } else {
+            requestNotificationPermission();
+        }
+    }
 
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-            if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-                notificationManager.notify(1052, builder.build());
-            }
-        });
+    private boolean hasNotificationPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestNotificationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, PERMISSION_REQUEST_CODE);
     }
 
     private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = getString(R.string.channel_id);
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
-            String channelId = getString(R.string.channel_id);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
             NotificationChannel channel = new NotificationChannel(channelId, name, importance);
             channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
+
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
     }
-
 }
